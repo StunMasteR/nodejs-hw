62802f3a4976aa44009ebc6a4c4399ef665ddf91
StunMasteR <162857412+StunMasteR@users.noreply.github.com>
2026-01-14 18:25:48 +0200

    dz

---
+++ src/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import pino from 'pino';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+    pino({
+      level: 'info',
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'HH:MM:ss',
+          ignore: 'pid,hostname',
+          messageFormat:
+            '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+          hideObject: true,
+        },
+      },
+    }),
+  app.use(cors());
+  app.use(helmet());
+  app.use(express.json({ limit: '5mb' }));
+
+app.get('/notes', (req, res) => {
+    res.status(200).json({ "message": "Retrieved all notes" });
+});
+
+app.get('/notes/:id', (req, res) => { const { noteId } = req.params;
+res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', () => {
+    throw new Error('Simulated server error');
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+  });
+
+  app.use((err, req, res, next) => {
+    const isProd = process.env.NODE_ENV === 'production';
+
+    res.status(500).json({
+      message: isProd
+        ? 'Something went wrong. Please try again later.'
+        : err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
