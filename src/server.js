244390086267645efea6f0f77840799b9651a2f5
StunMasteR <162857412+StunMasteR@users.noreply.github.com>
2026-01-16 01:31:32 +0200

    33

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import pino from 'pino';
+import pinoHttp from 'pino-http';
 
 import dotenv from 'dotenv';
 dotenv.config();
