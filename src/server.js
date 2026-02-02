d0dc609c8a312a4e3c61ba805df4fae7061476c3
StunMasteR <162857412+StunMasteR@users.noreply.github.com>
2026-02-02 19:29:51 +0200

    dz

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -15,13 +17,14 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(cors());
 app.use(helmet());
+app.use(cookieParser());
 app.use(express.json({ limit: '5mb' }));
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
 
-// обробка помилок від celebrate (валідація)
 app.use(errors());
 
 app.use(errorHandler);
